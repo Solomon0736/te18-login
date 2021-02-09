0e68cdefccdabd0c272fcda205fab61abfedac4c
solomon <solomon78@gmail.com>
2021-02-09 11:14:52 +0100

    post andring

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ // res.send('respond with a resource');
+ res.render('login', {title:'schollsoft'});
 });
+/* post login*/
+router.post('/', function(req, res, next) {
+  // res.send('respond with a resource');
+  res.render('login') 
+  console.log(req.body);
+
+  const username= req.body.username;
+  const password= req.body.password;
+
+  if(password == "tikabo"){
+    res.send('topsecret');
+  }
+
+ });
 
 module.exports = router;
