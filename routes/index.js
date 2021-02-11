34f6879667aad60e155071d46c7eb1d02140b57c
solomon <solomon78@gmail.com>
2021-02-11 09:35:53 +0100

    express-session

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/topsecret',function(req,res,next){
+    if(req.session.loggedin){
+    }else{
+        res.send('please login to view this page!');
+    }
+
+})
 
 module.exports = router;
