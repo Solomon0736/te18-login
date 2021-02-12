2552caa4106fb6ad92ee9a7970c3ab53b831b1e4
solomon <solomon78@gmail.com>
2021-02-12 09:37:41 +0100

    install dotenv,mysql,bcrypt hash

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const { query} = require('../models/db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
  // res.send('respond with a resource');
  res.render('login', {title:'schollsoft'});
 });
+
+router.get('/kryptan/:pwd', function(req, res, next) {
+  
+  const myPlaintextPassword = req.params.pwd;
+ 
+  bcrypt.hash(myPlaintextPassword, 10 , function(err, hash) {
+   
+    res.json({
+      pwd: hash
+    // Store hash in your password DB.
+    });
+  });
+
+});
+  
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+  res.render('login', {title:'schollsoft'});
+});
+
 /* post login*/
 router.post('/', function(req, res, next) {
   // res.send('respond with a resource');
