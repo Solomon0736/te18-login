79c702ae70a196bb044b6df0329737ef698ecf58
solomon <solomon78@gmail.com>
2021-02-12 08:42:58 +0100

    github

--- routes/login.js
+++ routes/login.js
@@ -9,25 +9,25 @@ router.get('/', function(req, res, next) {
 /* post login*/
 router.post('/', function(req, res, next) {
   // res.send('respond with a resource');
- /* res.render('login') */
   console.log(req.body);
 
   const username= req.body.username;
   const password= req.body.password;
 
   if(password == "tikabo"){
+
     req.session.loggedin = true;
     req.session.username= username;
-    res.redirect('/topscret')
+    res.redirect('/topsecret')
+
   }else{
     res.render(
       'login',
        {
          title:'Schoolsoft',
          error: 'fel!'
-         
-      });
-
+       }
+    );
   }
 
  });
